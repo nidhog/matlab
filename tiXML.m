e553fdec7a6f8bd5b42c024ff92a2d05c788b7b7
DanielEWilson <dan.wilson@mpfi.org>
2013-04-13 11:59:23 -0300

    Update tiXML.m

--- tiXML.m
+++ tiXML.m
@@ -1,3 +1,3 @@
 % get times from (non-triggered) 2P without showing figure
 xmlFileName = uigetfile('*.xml')
-[P, S, stimOn, stimID, ST] = getTimesXML(0, xmlFileName);
+[P, S, stimOn, stimID, ST, M] = getTimesXML(0, xmlFileName);
